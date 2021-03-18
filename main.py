2349b32eb54421eac41986d10b7faae53aa43ad2
lerahkp <52873528+lerahkp@users.noreply.github.com>
2021-03-18 09:38:24 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# 🚨 Don't change the code below 👇
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
 j=int(position[0])
 k=int(position[1])
 print(j)
@@ -30,6 +27,5 @@ elif(j==3 and k==2):
   row2[2]="X"
 elif(j==3 and k==3):
   row3[2]="X"
-#Write your code above this row 👆
-# 🚨 Don't change the code below 👇
+
 print(f"{row1}\n{row2}\n{row3}")
